cdfdba71bcac656c5bbcd58cea0f345382bc29cf
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-05-02 15:37:48 -0300

    Para saber mais numericos

---
+++ Tipo Primitivos/para_saber_mais_numericos.js
@@ -0,0 +1,17 @@
+// Valor PI
+
+const pi = Math.PI
+
+console.log(pi)
+
+// Valor pi com Duas casas decimais.
+
+const piDuasCasas = parseFloat(pi.toFixed(2))
+
+console.log(piDuasCasas)
+
+// Valor pi arredondado.
+
+const piArredondado = Math.round(pi)
+
+console.log(piArredondado)
