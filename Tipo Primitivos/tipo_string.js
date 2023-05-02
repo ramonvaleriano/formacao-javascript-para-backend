bc2162acbb5fdd758312209047c1cbb8cab1204f
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-05-02 15:58:07 -0300

    Aprendendo tipo string

---
+++ Tipo Primitivos/tipo_string.js
@@ -0,0 +1,24 @@
+// Strings
+
+const texto1 = "Olá mundo!"
+const texto2 = 'Ramon Valeriano'
+const senha = 'SenhaSupersenha123445234!@#%$#'
+const stringNUmber = '028574507453745'
+
+console.log(texto1)
+console.log()
+
+const citacao = 'Olá, vamos aprender "JS"\n'
+
+console.log(citacao)
+
+const outraCitacao = "Olá, 'NodeJS'"
+
+console.log(outraCitacao)
+
+// Concatenação (+)
+
+const meApresentando = 'Meu nome é: '
+const nome = 'Ramon Valeriano'
+
+console.log(meApresentando + nome)
