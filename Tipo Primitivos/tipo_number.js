c80d04bfa519b34ad423e7ecba2c84ffb41bd976
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-05-02 15:22:00 -0300

    Aprendendo sobre números flutuantes e NaN = Not a Number

--- Tipo Primitivos/tipo_number.js
+++ Tipo Primitivos/tipo_number.js
@@ -21,3 +21,20 @@ console.log(operacaoMatematicaMultiplicao)
 console.log(operacaoMatematicaDivisao)
 
 console.log(operacaoMatematicaSubtracao)
+
+console.log()
+
+// Ponto Flutuante
+
+const numeroPontoFlutuante = 3.3
+const numeroPontoFlutuanteSemZero = .5
+
+const novaOperacaoDivisao = operacaoMatematicaSoma / numeroPontoFlutuanteSemZero
+
+console.log(novaOperacaoDivisao)
+
+// NaN -> Not a Number (Não é um número)
+
+const meuNome = 'Ramon';
+
+console.log(meuNome * meuNumero)
