ff907afd54bd81c5c184be7bcdb38362f9d19ab5
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-05-02 15:00:06 -0300

    Começando no mundo do JavaScript

---
+++ Tipo Primitivos/tipo_number.js
@@ -0,0 +1,11 @@
+// Tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero
+
+console.log(operacaoMatematica)
