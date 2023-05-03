3b92146bdd769063094fc4cebf810970d8f54008
Ramon Valeriano <rrvaleriano@gmail.com>
2023-05-03 11:57:45 -0300

    Trabalhando com toLowerCase

--- Tipo Primitivos/para_saber_mais_strings.js
+++ Tipo Primitivos/para_saber_mais_strings.js
@@ -1,4 +1,13 @@
+
+// Testando string com booleanos
+
 const cidade = 'belo horizonte'
 const input = 'Belo Horizonte'
 
 console.log(cidade == input)
+
+// Mexendo com string para deixar todo minusculo.
+
+const novoInput = input.toLowerCase(input)
+
+console.log(cidade == novoInput)
