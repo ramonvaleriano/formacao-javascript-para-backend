d5c76b617ce160b5445bf04daa9688d1169a509c
Ramon Valeriano <rrvaleriano@gmail.com>
2023-05-04 15:39:50 -0300

    Tipo Boolean

---
+++ Tipo Primitivos/tipo_boolean.js
@@ -0,0 +1,13 @@
+primeiroNumero = 5
+segundoNumero = 10
+
+comparador1 = primeiroNumero === segundoNumero
+
+console.log(comparador1)
+
+terceiroNumero = 5
+
+comparador2 = terceiroNumero === primeiroNumero
+
+console.log(comparador2)
+
