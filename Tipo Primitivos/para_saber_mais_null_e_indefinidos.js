f729b4c852db91e63a34e1a612f26534524c2341
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-05-06 11:57:23 -0300

    Null e Indefinido

---
+++ Tipo Primitivos/para_saber_mais_null_e_indefinidos.js
@@ -0,0 +1,33 @@
+// Estudando os indefinidos.
+
+let input = null;
+
+if(input == null) {
+    console.log('Não há dados aqui');
+}
+else {
+    console.log(input)
+}
+
+// Diferença entre Null e Indefinido.
+
+input = null;
+let input2;
+
+console.log()
+console.log(input)
+console.log(input2)
+
+// Diferença entre == e ===
+
+console.log(input == input2)
+console.log(input === input2)
+
+// Para entendimento meu 
+
+let number1 = 10
+let number2 = "10"
+
+console.log('\n\n')
+console.log(number1 == number2)
+console.log(number1 === number2)
