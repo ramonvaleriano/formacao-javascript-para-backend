08feecb96fa3647e94586594f6500a4cce3f1075
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-05-06 12:18:36 -0300

    Variavel_let_2

--- Declarando Variaveis/var_let_const.js
+++ Declarando Variaveis/var_let_const.js
@@ -9,8 +9,17 @@ var area;
 
 // Let
 
+let forma = 'retângulo'
 let altura1 = 5;
 let comprimento1 = 7;
+let area1;
 
-area1 = altura1 * comprimento1
+if (forma == 'retângulo'){
+    area1 = altura1 * comprimento1
+}
+else {
+    area1 = (altura1 * comprimento1) / 2
+}
+
+console.log('\n\n')
 console.log(area1)
