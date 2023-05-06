cb8498017935d112230dfa95a0315e5c23b7a7aa
Ramon Valeriano <rrvaleriano@gmail.com>
2023-05-06 17:07:20 -0300

    Convertendo numeros para strings

---
+++ Declarando Variaveis/para_saber_mais_conversoes.js
@@ -0,0 +1,11 @@
+// Convertendo Numeros para Strings.
+
+let numberPhone = 1234556789
+let stringNumberPhone = String(numberPhone)
+let toStringNumberPhone = numberPhone.toString()
+
+console.log('Textando as conversões de números para string: ')
+console.log(numberPhone)
+console.log(stringNumberPhone)
+console.log(toStringNumberPhone)
+
