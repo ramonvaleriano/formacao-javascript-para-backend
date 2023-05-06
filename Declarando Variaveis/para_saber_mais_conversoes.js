89c846cf32258c9b18b3f3aaadbfc137acb3e9d5
Ramon Valeriano <rrvaleriano@gmail.com>
2023-05-06 17:15:25 -0300

    Convertendo booleano em string

--- Declarando Variaveis/para_saber_mais_conversoes.js
+++ Declarando Variaveis/para_saber_mais_conversoes.js
@@ -4,8 +4,21 @@ let numberPhone = 1234556789
 let stringNumberPhone = String(numberPhone)
 let toStringNumberPhone = numberPhone.toString()
 
-console.log('Textando as conversões de números para string: ')
+console.log('Testando as conversões de números para string: ')
 console.log(numberPhone)
 console.log(stringNumberPhone)
 console.log(toStringNumberPhone)
 
+// Convertendo Booleans para Strings.
+
+let dataBool = false
+let stringBool = String(dataBool)
+
+let dataBool1 = true
+let stringBool1 = dataBool.toString()
+
+console.log('\nTestando a conversão de booleano para String: ')
+console.log(dataBool)
+console.log(stringBool)
+console.log(dataBool1)
+console.log(stringBool1)
