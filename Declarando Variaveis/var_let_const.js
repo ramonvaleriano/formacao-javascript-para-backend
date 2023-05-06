190b36335492904e49cc0b32d2df31c7bf447b6e
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-05-06 12:12:07 -0300

    Variavel_let

--- Declarando Variaveis/var_let_const.js
+++ Declarando Variaveis/var_let_const.js
@@ -6,3 +6,11 @@ var comprimento = 2;
 area = altura * comprimento;
 console.log(area)
 var area;
+
+// Let
+
+let altura1 = 5;
+let comprimento1 = 7;
+
+area1 = altura1 * comprimento1
+console.log(area1)
