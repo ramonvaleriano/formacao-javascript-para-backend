68dbb8642d24bd793136d36f0a457fd8ff7aad7d
Ramon Valeriano <rrvaleriano@gmail.com>
2023-05-06 18:13:14 -0300

    Estudando os erros.

---
+++ JavaScript e NodeJs/erros_e_stracktrace.js
@@ -0,0 +1,13 @@
+// Erro para a inicialização const
+//const numero;
+const numero = 0;
+
+// Erro de sintaxe faltando )
+//console.log(numero
+
+console.log(numero)
+
+// Erro na declaração de uma variável não existente
+//console.log(variavel)
+variavel = 'Ramon'
+console.log(variavel)
