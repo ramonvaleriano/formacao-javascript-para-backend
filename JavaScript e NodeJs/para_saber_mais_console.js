4e878f53034c4bfe7546cec370433ef3c8bcfc06
Ramon Valeriano <rrvaleriano@gmail.com>
2023-05-07 15:46:13 -0300

    Para saber mais console

---
+++ JavaScript e NodeJs/para_saber_mais_console.js
@@ -0,0 +1,17 @@
+// Console Error.
+
+console.error('Isso daqui é um error!')
+
+// Console Table.
+
+console.table('Estou usando o table.')
+
+// Console time e timeEnd.
+
+console.time()
+console.log()
+console.timeEnd()
+
+// Console Trace.
+
+console.trace()
