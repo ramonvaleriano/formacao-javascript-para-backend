7e8c380378f568e468837a3a8f6bbf9b31fb30a3
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-05-08 10:33:34 -0300

    Tipando os comparadores

--- Operadores/comparacoes.js
+++ Operadores/comparacoes.js
@@ -8,3 +8,9 @@ console.log(numero == string)
 // ==== Comparação explicita.
 
 console.log(numero === string)
+
+// Tipo de variável typeof
+
+console.log(typeof numero)
+console.log(typeof string)
+
