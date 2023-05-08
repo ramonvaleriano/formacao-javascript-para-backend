f5fdf90d89d99f8433c1cab3d0a5e26a9d49d32e
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-05-08 10:48:53 -0300

    Para saber mais operadores

---
+++ Operadores/para_saber_mais_operadores.js
@@ -0,0 +1,31 @@
+// Operador "e"
+
+console.log('Operador E')
+
+let minhaVariavel1 = true
+let minhaVariavel2 = false
+let minhaVariavel3 = true
+
+let operacao1 = minhaVariavel1 && minhaVariavel2
+let operacao2 = minhaVariavel1 && minhaVariavel3
+let operacao3 = 2 != '2'
+let operacao4 = 2 !== '2'
+
+console.log(operacao1)
+console.log(operacao2)
+console.log(operacao3)
+console.log(operacao4)
+
+// Operador "ou"
+
+console.log('Operador OU')
+
+operacao1 = minhaVariavel1 || minhaVariavel2
+operacao2 = minhaVariavel1 || minhaVariavel3
+operacao3 = 2 != '2'
+operacao4 = 2 !== '2'
+
+console.log(operacao1)
+console.log(operacao2)
+console.log(operacao3)
+console.log(operacao4)
