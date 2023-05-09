e63ad88112b7ce1a6ee8060d5e4a381ba8405429
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-05-09 11:26:28 -0300

    Usando um função sem a passagem de parâmetros

--- Funcoes/funcoes.js
+++ Funcoes/funcoes.js
@@ -5,7 +5,15 @@ function imprimir_mensagem(texto){
     console.log(texto)
 }
 
-
 //2° Executando a função.
 imprimir_mensagem('Meu nome é:')
 imprimir_mensagem('Ramon Valeriano')
+
+
+// Funcão que não recebe parâmetros.
+function soma(){
+    const resultado = 2 + 2
+    return resultado
+}
+
+console.log(soma())
