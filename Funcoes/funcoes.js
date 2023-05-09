72f63e45a6a1f69e134217d6ba02aa2a4aa3a10c
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-05-09 11:22:07 -0300

    Estudando funções

---
+++ Funcoes/funcoes.js
@@ -0,0 +1,11 @@
+
+
+//1° Declarando uma função.
+function imprimir_mensagem(texto){
+    console.log(texto)
+}
+
+
+//2° Executando a função.
+imprimir_mensagem('Meu nome é:')
+imprimir_mensagem('Ramon Valeriano')
