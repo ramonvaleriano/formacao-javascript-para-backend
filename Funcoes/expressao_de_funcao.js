14366d8c4f609722753a89a6014e14de68446678
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-05-10 11:14:44 -0300

    Expressões de funções

---
+++ Funcoes/expressao_de_funcao.js
@@ -0,0 +1,13 @@
+// Declaração de função
+
+function minhaFuncao(nome){
+    // BLoco de construção
+}
+
+minhaFuncao('Ramon')
+
+// Expressão de função.
+
+const  soma = function(num1 , num2) {return num1 + num2}
+
+console.log(soma(1, 1))
