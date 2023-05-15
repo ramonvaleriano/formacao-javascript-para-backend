2d7bbebb3aca4ca76b3933962718e8854ea64654
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-05-15 15:58:50 -0300

    Para saber mais Matrizes

---
+++ Curso-JavaScript_Arrays/Alterando_arrays/para-saber-mais-matrizes.js
@@ -0,0 +1,7 @@
+const nomes = ['Ramon', 'Milla']
+const idade = [33, 32]
+const faculdade = ['Unifacs', 'UPE']
+
+const dadosNomesIdadeFaculdade = [nomes, idade, faculdade]
+
+console.log(`${dadosNomesIdadeFaculdade[0][1]} -> ${dadosNomesIdadeFaculdade[1][1]} -> ${dadosNomesIdadeFaculdade[2][1]}`)
