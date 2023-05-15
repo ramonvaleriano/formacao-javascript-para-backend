968c78c0e036d88cdc0bd9f33d1d9627cc4754eb
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-05-15 15:49:47 -0300

    Usando arrays duplos

---
+++ Curso-JavaScript_Arrays/Alterando_arrays/lista-duas-dimensoes.js
@@ -0,0 +1,9 @@
+// Como se manipular listas com mais de uma dimensão.
+
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const media = [10, 8, 7.5, 9];
+
+// Criando ums lista com duas listas, a partir das duas anteriores.
+
+const listaAlunosMedias = [alunos, media];
+
