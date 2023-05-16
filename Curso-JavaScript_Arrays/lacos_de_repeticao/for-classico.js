ef35792714508a26be2ee695b6863f4b218a3aa3
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-05-16 15:38:32 -0300

    For na sua forma mais básica

---
+++ Curso-JavaScript_Arrays/lacos_de_repeticao/for-classico.js
@@ -0,0 +1,10 @@
+/*Estrutarando o uso do mais básico do for.*/
+
+// Lista com dados para estudo.
+const numeros = [100, 200, 300, 400, 500, 600]
+
+// Usando o for, ou laço controlado.
+
+for (let indice = 0; indice < numeros.length; indice += 1){
+    console.log(numeros[indice])
+}
