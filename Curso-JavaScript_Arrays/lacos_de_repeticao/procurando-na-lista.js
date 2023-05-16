cd3675f66e0642dbf932ac6062d1980ad403388c
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-05-16 14:45:41 -0300

    Melhorando a função

--- Curso-JavaScript_Arrays/lacos_de_repeticao/procurando-na-lista.js
+++ Curso-JavaScript_Arrays/lacos_de_repeticao/procurando-na-lista.js
@@ -5,12 +5,21 @@ const medias = [6, 10, 10, 9, 7, 5.4];
 const listaAlunosMedias = [alunos, medias];
 
 // Função para validação de dado.
-function exibirNomeENota(nome, lista){
-    if (lista[0].include(nome)){
-        console.log(`\n${nome} já se encontra cadastrado na lista.`)
-    }
-    else{
-        console.log(`\nAluno não encontrado.`)
-    }
+function exibirNomeENota(nome, lista) {
+  console.log("\nValidando nome do alunos na lista: ");
+  if (lista[0].includes(nome)) {
+    console.log(`${nome} já se encontra cadastrado na lista.`);
+
+    const indice = lista[0].indexOf(nome)
+    const nota = lista[1][indice]
+
+    console.log(`A nota do aluno foi ${nota}`)
+
+  } else {
+    console.log(`Aluno não encontrado.`);
+  }
 }
 
+// Chamando a função para validação dos alunos.
+exibirNomeENota("Ramon", listaAlunosMedias);
+exibirNomeENota("Andréa", listaAlunosMedias);
