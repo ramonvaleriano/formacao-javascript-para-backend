5177c91c8dcd4a707649245f5a67beded8f1d0fa
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-05-16 14:37:47 -0300

    Criando função para validação de aluno

--- Curso-JavaScript_Arrays/lacos_de_repeticao/procurando-na-lista.js
+++ Curso-JavaScript_Arrays/lacos_de_repeticao/procurando-na-lista.js
@@ -1,5 +1,16 @@
+// Listas para trabalhar.
 const alunos = ["Ramon", "Milla", "Gael", "Dante", "Rayan", "Caio"];
 const medias = [6, 10, 10, 9, 7, 5.4];
 
 const listaAlunosMedias = [alunos, medias];
+
+// Função para validação de dado.
+function exibirNomeENota(nome, lista){
+    if (lista[0].include(nome)){
+        console.log(`\n${nome} já se encontra cadastrado na lista.`)
+    }
+    else{
+        console.log(`\nAluno não encontrado.`)
+    }
+}
 
