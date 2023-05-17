e5196c750a2fada73c423f883cb01b08cb4b7772
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-05-17 11:44:59 -0300

    Calculando a media usando forEach

--- Curso-JavaScript_Arrays/funcoes_callback/media-com-foreach.js
+++ Curso-JavaScript_Arrays/funcoes_callback/media-com-foreach.js
@@ -2,6 +2,18 @@
 const notas = [10, 6.5, 8, 7.5];
 
 // Testando saída.
-notas.forEach(function (){
-    console.log('Olá')
-})
+notas.forEach(function () {
+  console.log("Olá");
+});
+
+// Fazendo a média com o forEach agora.
+let somaNumeros = 0;
+let media = 0;
+
+notas.forEach(function (nota) {
+  somaNumeros += nota;
+});
+
+media = somaNumeros / notas.length;
+
+console.log(`A media das notas: ${media}`);
