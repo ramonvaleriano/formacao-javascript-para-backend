18bf80dc46e853e85eaafcf57554816d8fc26fca
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-05-17 11:04:11 -0300

    Saltando a cada duas linhas e exibindo um valor acima de 300

--- Curso-JavaScript_Arrays/lacos_de_repeticao/para-saber-mais-possibilidades-do-for.js
+++ Curso-JavaScript_Arrays/lacos_de_repeticao/para-saber-mais-possibilidades-do-for.js
@@ -6,3 +6,15 @@ const numeros = [100, 200, 300, 400, 500, 600]
 for(i = numeros.length - 1; i >= 0; i --){
     console.log(numeros[i])
 }
+
+// Saltando a cada duas linhas.
+console.log('\nPulando um valor:')
+for(i = numeros.length - 1; i >= 0; i-=2 ){
+    console.log(numeros[i])
+}
+
+// Saltando a cada duas linhas e exibindo um valor acima de 300.
+console.log('\nPulando uma linha e sendo maior que 300: ')
+for(i=numeros.length - 1; numeros[i] >= 300; i-=2){
+    console.log(numeros[i])
+}
