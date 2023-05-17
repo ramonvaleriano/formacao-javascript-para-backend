29db6c693800d3336c10b59448914f4846f9597d
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-05-17 10:59:04 -0300

    Percorrendo uma array de trás para frente

---
+++ Curso-JavaScript_Arrays/lacos_de_repeticao/para-saber-mais-possibilidades-do-for.js
@@ -0,0 +1,8 @@
+/*Testando outras possibilidades para o uso do for*/
+
+// Fazer o loop de forma invertida.
+const numeros = [100, 200, 300, 400, 500, 600]
+
+for(i = numeros.length - 1; i >= 0; i --){
+    console.log(numeros[i])
+}
