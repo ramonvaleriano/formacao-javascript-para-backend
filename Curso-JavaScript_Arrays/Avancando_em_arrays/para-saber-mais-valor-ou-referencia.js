add3b3b321d93818524f2fd214e1dee161f705ea
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-05-25 16:44:03 -0300

    Para saber mais, atribuindo valores

---
+++ Curso-JavaScript_Arrays/Avancando_em_arrays/para-saber-mais-valor-ou-referencia.js
@@ -0,0 +1,16 @@
+/*Para saber mais, valor ou referência.*/
+
+// Copiar com tipos primitivos.
+let num1 = 10
+let num2 = num1
+
+console.log('Testando variáveis antes orifinais:')
+console.log(`Num1 ${num1}`)
+console.log(`Num2 ${num2}`)
+
+num2+=10
+num1+=5
+
+console.log('Testando variáveis após auterações:')
+console.log(`Num1 ${num1}`)
+console.log(`Num2 ${num2}`)
