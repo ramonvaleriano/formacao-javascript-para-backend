9830b90110605ce841aee8143dce89c0c85b10e5
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-05-25 15:56:55 -0300

    Formando de forma adequada

--- Curso-JavaScript_Arrays/Avancando_em_arrays/para-saber-mais-reduce.js
+++ Curso-JavaScript_Arrays/Avancando_em_arrays/para-saber-mais-reduce.js
@@ -2,24 +2,24 @@
 
 // Usando dados para entender como funciona o reduce.
 // Lista de dados
-const numeros = [43, 50, 65, 12]
+const numeros = [43, 50, 65, 12];
 
 // Usando reduce com o arrow function
-const soma = numeros.reduce((acc, numero) => acc + numero,0)
+const soma = numeros.reduce((acc, numero) => acc + numero, 0);
 
-console.log(`A soma dos números: ${soma}`)
+console.log(`A soma dos números: ${soma}`);
 
 // Usando com uma função criada internamente.
-const somaNova = numeros.reduce(function (acc, numero){
-    return acc + numero
-}, 0)
+const somaNova = numeros.reduce(function (acc, numero) {
+  return acc + numero;
+}, 0);
 
-console.log(`A soma dos números: ${somaNova}`)
+console.log(`A soma dos números: ${somaNova}`);
 
 // Usando com uma função externa.
-function somaDosNumeros(acc, numero){
-    return acc + numero
+function somaDosNumeros(acc, numero) {
+  return acc + numero;
 }
 
-const somaNovaDeNovo = numeros.reduce(somaDosNumeros, 0)
-console.log(`A soma dos número: ${somaNovaDeNovo}`)
+const somaNovaDeNovo = numeros.reduce(somaDosNumeros, 0);
+console.log(`A soma dos número: ${somaNovaDeNovo}`);
