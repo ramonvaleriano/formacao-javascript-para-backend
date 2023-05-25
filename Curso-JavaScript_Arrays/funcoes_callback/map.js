d24047ae8c585861eb15ec1ee1d1061ad8ae6978
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-05-25 09:59:59 -0300

    Usando método map

---
+++ Curso-JavaScript_Arrays/funcoes_callback/map.js
@@ -0,0 +1,9 @@
+/*Aprendendo a usar o MAP*/
+
+// Uma forma simples de usar o map no lugar do forEach.
+const notas = [6, 9.3, 10, 6.8, 7.6];
+
+const notasAtualizadas = notas.map((nota) => {
+  return nota + 1 >= 10 ? 10 : nota
+});
+console.log(notasAtualizadas);
