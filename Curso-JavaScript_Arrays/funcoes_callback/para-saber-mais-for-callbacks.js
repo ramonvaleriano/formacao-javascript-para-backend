b4acf2e8640d1bfeb75ded576038f6934ac20d7d
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-05-25 09:28:26 -0300

    Revisando alguns contextos de for

--- Curso-JavaScript_Arrays/funcoes_callback/para-saber-mais-for-callbacks.js
+++ Curso-JavaScript_Arrays/funcoes_callback/para-saber-mais-for-callbacks.js
@@ -3,20 +3,33 @@
 const lista = [1, 2, 3, 4, 5];
 
 // Usando for de forma convencional.
-console.log('Usando for convencional.')
+console.log("Usando for convencional.");
 for (let indice = 0; indice < lista.length; indice++) {
-  console.log((lista[indice]));
+  console.log(lista[indice]);
 }
 
 // Usando for com dois indices.
-console.log('\nUsando for com dois indices.')
-for (let i =0, j=0; i < lista.length; i++, j++){
-    console.log(lista[i] + j)
+console.log("\nUsando for com dois indices.");
+for (let i = 0, j = 0; i < lista.length; i++, j++) {
+  console.log(lista[i] + j);
 }
 
 // Usando for of.
-console.log('\nUsando for of')
-for (elemento of lista){
-    console.log(elemento)
+console.log("\nUsando for of");
+for (elemento of lista) {
+  console.log(elemento);
 }
 
+// Usando for of para criar uma somatória de todos os elementos.
+console.log("\nUsando for of para a somatória de todos os elementos.");
+let soma = 0;
+for (elemento of lista) {
+  soma += elemento;
+}
+console.log(`A soma: ${soma}`);
+
+// Usando forEache com um arrow_function.
+console.log("\nUsando forEach com uma arrow function.");
+let somatoria = 0;
+lista.forEach((numero) => (somatoria += numero));
+console.log(`A somatória: ${somatoria}`);
