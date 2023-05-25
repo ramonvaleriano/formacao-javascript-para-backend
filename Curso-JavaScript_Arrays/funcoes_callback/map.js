7b32a627d8a8e8b8955e54665a16f2b8f78f47b9
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-05-25 10:13:32 -0300

    Ajustando dados do map

--- Curso-JavaScript_Arrays/funcoes_callback/map.js
+++ Curso-JavaScript_Arrays/funcoes_callback/map.js
@@ -4,6 +4,6 @@
 const notas = [6, 9.3, 10, 6.8, 7.6];
 
 const notasAtualizadas = notas.map((nota) => {
-  return nota + 1 >= 10 ? 10 : nota
+  return nota + 1 >= 10 ? 10 : nota + 1
 });
 console.log(notasAtualizadas);
