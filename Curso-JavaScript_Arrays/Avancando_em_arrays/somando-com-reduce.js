5e0493c54b8148419169dba4284226f40235eced
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-05-25 14:36:05 -0300

    Começando a usar reduce

---
+++ Curso-JavaScript_Arrays/Avancando_em_arrays/somando-com-reduce.js
@@ -0,0 +1,22 @@
+/*Somando com reduce*/
+
+// Dados para manipulação.
+const salaJS = [7, 8, 8, 7, 10, 6.5, 4, 10, 7];
+
+const salaJava = [6, 5, 8, 9, 5, 6];
+
+const salaPython = [7, 3.5, 8, 9.5];
+
+// Usando reduce para somar todas as notas.
+function calculaMedia(notasDaSala){
+    const somaDasNotas = notasDaSala.reduce((acumulador, nota) => {
+        return acumulador + nota
+    }, 0)
+
+    const media = somaDasNotas / notasDaSala.length
+
+    console.log(`A média das notas: ${media}`)
+    return media
+}
+
+calculaMedia(salaJS)
