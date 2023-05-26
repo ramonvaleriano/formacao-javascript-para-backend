38629e0457b74b0a449323d5f90f8705ad9690fd
Ramon Valeriano <rrvaleriano@gmail.com>
2023-05-25 23:13:11 -0300

    Usando set para remover elementos repetidos em um array

---
+++ Curso-JavaScript_Arrays/Avancando_em_arrays/removendo-repstidos.js
@@ -0,0 +1,7 @@
+/*Removendo os elementos repetidos*/
+
+const nomes = ["Ana", "Clara", "Maria", "Maria", "João", "João", "João"];
+
+const nomesAtualizados = [... new Set(nomes)];
+
+console.log(nomesAtualizados)
