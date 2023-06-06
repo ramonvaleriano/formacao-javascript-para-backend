afceb706c64ef5c6df02cf6644e0afa114208503
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-06-06 15:43:29 -0300

    Começando a estudar objetos dentro de objetos

---
+++ Curso-JavaScript_Objetos/Manipulando_objetos/objetos-em-objetos.js
@@ -0,0 +1,9 @@
+/*Objeto dentro de outro objeto.*/
+
+// Dados para estudo.
+const cliente = {
+    nome: "Ramon",
+    idade: "33",
+    email: "ramonramon@valeriano.com",
+    telefone: ["71 9 9277-4903", "71 9 1234-1234"],
+  };
