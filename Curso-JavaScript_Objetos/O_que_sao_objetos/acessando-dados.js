62b9f2e025eb4fe5358aa6a96f3d13dc2bc76c67
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-06-06 13:55:34 -0300

    Acessando dados por colchetes

--- Curso-JavaScript_Objetos/O_que_sao_objetos/acessando-dados.js
+++ Curso-JavaScript_Objetos/O_que_sao_objetos/acessando-dados.js
@@ -3,16 +3,16 @@
 // Como acessar um objeto.
 
 const cliente = {
-    nome: 'Ramon',
-    idade: 33,
-    cpf: "122333444455555",
-    email: 'ramonramon@email.com'
-}
+  nome: "Ramon",
+  idade: 33,
+  cpf: "122333444455555",
+  email: "ramonramon@email.com",
+};
 
-console.log('Testando como acessar os dados de um objeto: ')
-console.log(`Nome: ${cliente["nome"]}`)
-console.log(`Idade: ${cliente.idade}`)
+console.log("Testando como acessar os dados de um objeto: ");
+console.log(`Nome: ${cliente["nome"]}`);
+console.log(`Idade: ${cliente.idade}`);
 
 // Como acessar uma substring.
 
-console.log(`Os três primeiros número do cpf: ${cliente.cpf.substring(0, 3)}`)
+console.log(`Os três primeiros número do cpf: ${cliente.cpf.substring(0, 3)}`);
