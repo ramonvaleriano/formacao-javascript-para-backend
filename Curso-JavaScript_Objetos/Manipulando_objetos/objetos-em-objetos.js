4c0074e174ef54f945c7b4e0b6cc46c4d9cc7977
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-06-06 16:24:31 -0300

    Objeto dentro de um objeto

--- Curso-JavaScript_Objetos/Manipulando_objetos/objetos-em-objetos.js
+++ Curso-JavaScript_Objetos/Manipulando_objetos/objetos-em-objetos.js
@@ -5,5 +5,17 @@ const cliente = {
     nome: "Ramon",
     idade: "33",
     email: "ramonramon@valeriano.com",
-    telefone: ["71 9 9277-4903", "71 9 1234-1234"],
+    telefone: ["71 9 9277-4903", "71 9 1234-1234"]
   };
+
+
+// Adicionando um objeto dentro do objeto.
+cliente.endereco = {
+  rua: 'Aquela rua lá',
+  numero: 'O que tiver escrito na porta',
+  apartamento: true,
+  complemento: "Ai você que saber demais"
+}
+
+console.log('Objeto dentro do objeto: ')
+console.log(cliente)
