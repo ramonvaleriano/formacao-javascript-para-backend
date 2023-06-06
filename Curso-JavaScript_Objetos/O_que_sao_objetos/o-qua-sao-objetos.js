d514ddcab2d0ce8534ceec132fb8a2c8f0d2d2cc
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-06-06 11:09:40 -0300

    O que são objetos

---
+++ Curso-JavaScript_Objetos/O_que_sao_objetos/o-qua-sao-objetos.js
@@ -0,0 +1,11 @@
+/*O que são Objetos.*/
+
+// Como funciona a chave.
+
+const objetoPessoa = {
+    nome: 'Ramon',
+    idade: 33
+}
+
+console.log('Testando o que é um objeto: ')
+console.log(objetoPessoa)
