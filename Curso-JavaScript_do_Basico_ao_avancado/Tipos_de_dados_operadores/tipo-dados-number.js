5f53e4b195e32a9a179c1250778fe02fcab2c73e
Ramon Valeriano <rrvaleriano@gmail.com>
2023-06-14 21:44:11 -0300

    Tipos de dados, number

---
+++ Curso-JavaScript_do_Basico_ao_avancado/Tipos_de_dados_operadores/tipo-dados-number.js
@@ -0,0 +1,9 @@
+/*Tipo de dados: Number*/
+
+// console.log() --> é usado para exibir alguma mensagem, texto.
+// typeof é usado para exibir o tipo de dado
+
+console.log('Testando alguns tipos de dados, number: ')
+console.log(typeof 18)
+console.log(typeof 1.5)
+console.log(typeof -5)
