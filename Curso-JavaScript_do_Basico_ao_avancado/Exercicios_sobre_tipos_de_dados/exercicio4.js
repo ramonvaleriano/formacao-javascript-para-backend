57add57d14620936ed1714497b4a36a099bee5a8
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-06-15 21:49:47 -0300

    Exercicio 04 de tipos de dados

---
+++ Curso-JavaScript_do_Basico_ao_avancado/Exercicios_sobre_tipos_de_dados/exercicio4.js
@@ -0,0 +1,16 @@
+/*Exercício 04*/
+
+/*
+    Escreva três comparações com operadores booleans.
+
+    com and, or e not.
+*/
+
+console.log("Exercício4");
+
+console.log(true && true);
+console.log(true && false);
+console.log(false || false);
+console.log(true || false);
+console.log(!true);
+console.log(!false);
