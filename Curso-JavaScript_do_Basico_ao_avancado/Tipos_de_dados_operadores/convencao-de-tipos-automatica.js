4353fc2c9fa18b5d78709b7c7c1ebdb5022bd114
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-06-15 21:22:34 -0300

    Convesão automática de dados

---
+++ Curso-JavaScript_do_Basico_ao_avancado/Tipos_de_dados_operadores/convencao-de-tipos-automatica.js
@@ -0,0 +1,12 @@
+/*Convenção de tipo automática.*/
+
+console.log("5 * null")
+console.log(5 * null)
+console.log('"5" - 2')
+console.log("5" - 2)
+console.log('"5" + 1')
+console.log("5" + 1)
+console.log('"dois" * "três"')
+console.log("dois" * "três")
+console.log("'Ramon ' + 'Valeriano'")
+console.log('Ramon ' + 'Valeriano')
