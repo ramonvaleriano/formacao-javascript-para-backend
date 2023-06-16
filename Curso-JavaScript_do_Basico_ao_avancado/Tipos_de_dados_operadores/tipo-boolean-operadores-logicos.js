c795059c6ab97747b063ea0c1300b0fc97b8c0bc
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-06-15 21:05:47 -0300

    Operadores logicos na pratica

---
+++ Curso-JavaScript_do_Basico_ao_avancado/Tipos_de_dados_operadores/tipo-boolean-operadores-logicos.js
@@ -0,0 +1,21 @@
+/*Boolean, Operadores Lógicos*/
+
+console.log("true && true");
+console.log(true && true);
+console.log("true && false");
+console.log(true && false);
+console.log("true || false");
+console.log(true || false);
+console.log("false || false");
+console.log(false || false);
+
+console.log("5 > 3 && 19 > 3");
+console.log(5 > 3 && 19 > 3);
+console.log("3 > 5 && 10 > 5");
+console.log(3 > 5 && 10 > 5);
+console.log("3 === 3 && 'Ramon' == 'Ramon'");
+console.log(3 === 3 && "Ramon" == "Ramon");
+console.log("'Ramon' === 'Milla' || false");
+console.log("Ramon" === "Milla" || false);
+console.log("!(true && true)");
+console.log(!(true && true));
