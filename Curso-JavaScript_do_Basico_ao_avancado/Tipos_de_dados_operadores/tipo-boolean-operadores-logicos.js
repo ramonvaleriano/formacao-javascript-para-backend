5d3c960d7393113c30bacb1b4178558383aa96e7
Ramon Valeriano <rrvaleriano@gmail.com>
2023-06-17 13:24:17 -0300

    Boolean, operadores logicos

---
+++ Curso-JavaScript_do_Basico_ao_avancado/Tipos_de_dados_operadores/tipo-boolean-operadores-logicos.js
@@ -0,0 +1,15 @@
+/*Boolean, operadores lógicos.*/
+
+console.log('Boolean, operadores lógicos: ')
+console.log('true && true')
+console.log(true && true)
+console.log('true && false')
+console.log(true && false)
+console.log('true || true')
+console.log(true || true)
+console.log('true || false')
+console.log(true || false)
+console.log('!true')
+console.log(!true)
+console.log('!false')
+console.log(!false)
