f01da0b3dc3353f2cf73ae6c09a8e2c153372f68
Ramon Valeriano <rrvaleriano@gmail.com>
2023-06-17 15:00:35 -0300

    Leitura de dados via terminanl

--- Curso-JavaScript_do_Basico_ao_avancado/Estrutura_de_programação/funcao-prompit.js
+++ Curso-JavaScript_do_Basico_ao_avancado/Estrutura_de_programação/funcao-prompit.js
@@ -2,17 +2,17 @@
 
 //let nome = prompt("Qual a sua idade: ");
 
-var readline = require('readline');
+var readline = require("readline");
 
 var leitor = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
-let meuNome = ''
+let meuNome = "";
 
 leitor.question("Qual é o seu nome? ", (nome) => {
-    meuNome = nome
-    console.log(`Meu nome é: ${meuNome}`);
-    leitor.close()
-})
+  meuNome = nome;
+  console.log(`Meu nome é: ${meuNome}`);
+  leitor.close();
+});
