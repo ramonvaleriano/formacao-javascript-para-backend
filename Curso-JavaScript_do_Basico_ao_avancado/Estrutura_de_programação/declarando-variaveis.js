3aa6d263ced01a8840334dc9f10997c3feed02a8
Ramon Valeriano <rrvaleriano@gmail.com>
2023-06-17 13:44:40 -0300

    Declarando variáveis

---
+++ Curso-JavaScript_do_Basico_ao_avancado/Estrutura_de_programação/declarando-variaveis.js
@@ -0,0 +1,10 @@
+/*Declerando variáveis.*/
+
+var nome = "Ramon"
+const sobrenome = 'Valeriano'
+
+console.log('Testando var e const:')
+console.log(`Nome completo: ${nome} ${sobrenome}`)
+
+nome = 'Milla'
+console.log(`Nome completo: ${nome} ${sobrenome}`)
