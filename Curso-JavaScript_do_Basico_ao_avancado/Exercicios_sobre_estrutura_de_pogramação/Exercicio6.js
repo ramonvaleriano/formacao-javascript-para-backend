6c1e838d4e9a069fd7465db39e79b763c2e9ee95
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-06-19 15:57:28 -0300

    Exercicio 06

---
+++ Curso-JavaScript_do_Basico_ao_avancado/Exercicios_sobre_estrutura_de_pogramação/Exercicio6.js
@@ -0,0 +1,15 @@
+/*
+    Armazene valores em variáveis com cada um dos tipos de dados vistos;
+
+    String, Boolean e Number.
+*/
+
+const nome = 'Ramon'
+const sobreNome = 'Valeriano'
+const idade = 33
+const empregado = true
+
+console.log(`Nome: ${nome} ${sobreNome}, idade: ${idade}, empregado: ${true}`)
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof empregado)
