e829fc35eb3f14aaf47eeb768a42d5ee964ec809
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-06-19 15:30:31 -0300

    Usando icrementação resumida

---
+++ Curso-JavaScript_do_Basico_ao_avancado/Estrutura_de_programação/incrementacao.js
@@ -0,0 +1,17 @@
+/*Incrementação de forma resumida.*/
+
+let number = 1;
+console.log(`Temos a variável number com o valor: ${number}`);
+console.log(`Vamos somar 1 usando o ++`);
+
+number++;
+console.log(`Temos a variável number com o valor: ${number}`);
+
+number += 5;
+console.log(`Vamos soamr 5 usando o += 5: ${number}`);
+
+number--;
+console.log(`Vamos subtrair 1 usando -- : ${number}`);
+
+number -= 2;
+console.log(`Vamos subtrair 2 usando -= 2: ${number}`);
