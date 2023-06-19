add30afae0b5521f6d2017cdb1944c21cbc650bc
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-06-19 11:44:14 -0300

    Estrutura else if

---
+++ Curso-JavaScript_do_Basico_ao_avancado/Estrutura_de_programação/estrutura-else-if.js
@@ -0,0 +1,17 @@
+/*Usando estrutura else if*/
+
+let numberOne = 3;
+let numbertwo = 5;
+
+if ((numberOne + numbertwo) == 3){
+    console.log('A soma dos dois números é igual a 3.')
+}
+else if ((numberOne + numbertwo) > 3){
+    console.log('A soma dos dois números é maior que 3.')
+}
+else if((numberOne + numbertwo) < 3){
+    console.log('A soma dos dois números é menor que 3')
+}
+else {
+    console.log('As condições não se encaixão em nenhuma acima.')
+}
