85dea10de4b223620701c04550c5b6f83563283b
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-06-19 13:54:24 -0300

    Estrutaura de repetição while

---
+++ Curso-JavaScript_do_Basico_ao_avancado/Estrutura_de_programação/estrutura-while.js
@@ -0,0 +1,20 @@
+/*Estrutura de Repetição While*/
+
+// Criando um contador de trás para frente.
+console.log("Criando um contador de trás para frente.");
+
+let x = 10;
+
+while (x > 0) {
+  console.log(x);
+  x -= 1;
+}
+
+// Criando um contado em ordem crescente.
+console.log("Criando um contado de ordem crescente.");
+
+x = 1;
+while (x <= 10) {
+  console.log(x);
+  x += 1;
+}
