4ed1c3530ad461fae7560fd3e898b996d6d131b0
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-06-19 11:38:21 -0300

    Estudando a estrutura else

---
+++ Curso-JavaScript_do_Basico_ao_avancado/Estrutura_de_programação/estrutura-else.js
@@ -0,0 +1,10 @@
+/*Usando estrutura else.*/
+
+let nome = 'Ramon'
+
+if (nome == 'Ramon'){
+    console.log('O seu nome é Ramon como no acionado.')
+}
+else{
+    console.log('Um nome diferente!')
+}
