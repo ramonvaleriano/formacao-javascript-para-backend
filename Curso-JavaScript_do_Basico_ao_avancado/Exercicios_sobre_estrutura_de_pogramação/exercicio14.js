f763501ac085576e5a19fe111683d42d65cdcfb9
Ramon Valeriano <rrvaleriano@gmail.com>
2023-06-19 21:27:40 -0300

    Exercício 14

---
+++ Curso-JavaScript_do_Basico_ao_avancado/Exercicios_sobre_estrutura_de_pogramação/exercicio14.js
@@ -0,0 +1,16 @@
+/*
+    Escreva um loop for ou while que exiba qual número é impar e qual número é par.
+
+    O contador deve ir de 0 a 50.
+*/
+
+
+for (let i = 0; i <=50; i++){
+  if (i % 2 == 0 ){
+    console.log(`O númeor: ${i} é Par!`)
+  }
+  else{
+    console.log(`O número: ${i} é Impar!`)
+  }
+}
+
