e205a2f0efeedb0424fc62009683645aa742e716
Ramon Valeriano <rrvaleriano@gmail.com>
2023-06-20 22:11:01 -0300

    Arrow function

---
+++ Curso-JavaScript_do_Basico_ao_avancado/Funções/funcao-arrow-function.js
@@ -0,0 +1,10 @@
+/*Estudando arrow funcition*/
+
+const numeroPar = (numero) => {
+    return numero % 2 == 0
+}
+
+console.log(numeroPar(4))
+console.log(numeroPar(5))
+console.log(numeroPar(6))
+console.log(numeroPar(7))
