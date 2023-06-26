98ac41ff67365e93c6cd91aa62e0d3af7e369dfb
Ramon Valeriano <rrvaleriano@gmail.com>
2023-06-26 19:50:53 -0300

    Exercicio01 da funções

---
+++ Curso-JavaScript_do_Basico_ao_avancado/Exercicios_sobre_funcoes/exercicio01.js
@@ -0,0 +1,26 @@
+/*
+    Exercício 01:
+
+    Uma funçaõ que imprima 'Hello Worlrd'
+*/
+
+// Usando função convencional
+function mesagemConvencional(){
+    console.log('Hello World!')
+}
+
+mesagemConvencional()
+
+// Usando variável para uma função
+const mensagemVariavel = function(){
+    console.log('Hello World!')
+}
+
+mensagemVariavel()
+
+// Usando Arrow Function
+const mesagemArrowFuncion = () => {
+    console.log('Hello World!')
+}
+
+mesagemArrowFuncion()
