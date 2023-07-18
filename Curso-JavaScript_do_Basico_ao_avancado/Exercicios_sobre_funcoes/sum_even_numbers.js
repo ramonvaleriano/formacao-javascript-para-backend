6f1cf133bf64b993b64b6018a40f647c1ec5c30e
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-07-18 09:52:49 -0300

    Soma de número pares na lista

--- Curso-JavaScript_do_Basico_ao_avancado/Exercicios_sobre_funcoes/sum_even_numbers.js
+++ Curso-JavaScript_do_Basico_ao_avancado/Exercicios_sobre_funcoes/sum_even_numbers.js
@@ -11,6 +11,17 @@
     Dica: Para verificar se um número é par, você pode usar a expressão numero % 2 === 0, onde % é o operador de módulo que retorna o resto da divisão.
 */
 
-function sumEvenNumbers(listNumbers){
-    continue
+function sumEvenNumbers(listNumbers) {
+  let sumEven = 0;
+  listNumbers.map((item) => {
+    if (item % 2 == 0) {
+      sumEven += item;
+    }
+  });
+  return sumEven;
 }
+
+listNumbers = [3, 2, 87, 32, 53, 67, 38];
+const result = sumEvenNumbers(listNumbers);
+
+console.log(result);
