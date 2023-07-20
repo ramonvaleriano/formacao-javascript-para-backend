a24fb1ee480d41102b1b64191114d1e8161e94cf
Ramon Valeriano <rrvaleriano@gmail.com>
2023-07-19 21:25:56 -0300

    Esutando métodos

---
+++ Curso-JavaScript_do_Basico_ao_avancado/Arrays_e_Objetos/metodos.js
@@ -0,0 +1,24 @@
+/*
+    Métodos
+*/
+
+// Estudando métodos.
+console.log("Estudando métodos");
+
+let marca = "Nike";
+
+let tipando = typeof marca.toUpperCase();
+let minusculo = marca.toLowerCase();
+let marca2 = marca.toLowerCase();
+
+console.log(tipando);
+console.log(minusculo);
+console.log(typeof marca2);
+
+/*
+    Observação:
+
+    string.propriedade
+    string.metodo()
+
+*/
