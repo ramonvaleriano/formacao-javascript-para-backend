1e17740752d0ca5aaadc7d944287107586c14f34
Ramon Valeriano <rrvaleriano@gmail.com>
2023-07-19 21:35:26 -0300

    Função dentro do objeto

--- Curso-JavaScript_do_Basico_ao_avancado/Arrays_e_Objetos/objetos.js
+++ Curso-JavaScript_do_Basico_ao_avancado/Arrays_e_Objetos/objetos.js
@@ -14,3 +14,16 @@ let pessoa = {
 console.log(pessoa.nome);
 console.log(pessoa["profissao"]);
 console.log(pessoa.idade);
+
+// Objeto com uma string dentro.
+console.log('Validando um objeto com uma função dentro.')
+let cachorro = {
+    patas: 4,
+    nome: 'Dog',
+    latir: function(){console.log('Au Au')}
+}
+
+console.log('O que há dentro do objeto Cachorro')
+console.log(cachorro)
+console.log('Usando a função interna do objeto: ')
+console.log(cachorro.latir())
