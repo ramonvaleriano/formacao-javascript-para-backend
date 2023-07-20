3e021303a82e040aae16e538cb7b30923d2a571b
Ramon Valeriano <rrvaleriano@gmail.com>
2023-07-19 21:29:04 -0300

    Estudando objetos

---
+++ Curso-JavaScript_do_Basico_ao_avancado/Arrays_e_Objetos/objetos.js
@@ -0,0 +1,16 @@
+/*
+    Objetos
+*/
+
+// Estudando objetos.
+console.log("Estudando os objetos");
+
+let pessoa = {
+  nome: "Ramon",
+  profissao: "Programador",
+  idade: 33,
+};
+
+console.log(pessoa.nome);
+console.log(pessoa["profissao"]);
+console.log(pessoa.idade);
