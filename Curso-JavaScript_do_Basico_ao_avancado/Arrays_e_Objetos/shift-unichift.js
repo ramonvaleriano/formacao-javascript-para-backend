198068e81ef0758a95e4ad0674b1dea95f7606ad
Ramon Valeriano <rrvaleriano@gmail.com>
2023-07-31 22:48:53 -0300

    Adicionando dados com unishift

--- Curso-JavaScript_do_Basico_ao_avancado/Arrays_e_Objetos/shift-unichift.js
+++ Curso-JavaScript_do_Basico_ao_avancado/Arrays_e_Objetos/shift-unichift.js
@@ -12,3 +12,8 @@ let primeiroeElemento = frutas.shift();
 console.log("Como ficou a lista: ");
 console.log(frutas);
 console.log(`Dado que foi removido: ${primeiroeElemento}`);
+
+// Adicionando dado com unishift.
+frutas.unshift("Acerola");
+console.log("Como ficou a lista: ");
+console.log(frutas);
