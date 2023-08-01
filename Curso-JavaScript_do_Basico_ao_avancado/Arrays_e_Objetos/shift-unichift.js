915452a32e7a76e9d8eace8f0d9670505cc38a73
Ramon Valeriano <rrvaleriano@gmail.com>
2023-07-31 22:44:00 -0300

    Removendo dado com shift

---
+++ Curso-JavaScript_do_Basico_ao_avancado/Arrays_e_Objetos/shift-unichift.js
@@ -0,0 +1,14 @@
+/*
+    Usando o shift e unishift.
+*/
+
+// Lista ou Array.
+let frutas = ["Maçã", "Uva", "Melão"];
+console.log("Como é a lista inicialmente: ");
+console.log(frutas);
+
+// Removenvo o primeiro elemento da lista.
+let primeiroeElemento = frutas.shift();
+console.log("Como ficou a lista: ");
+console.log(frutas);
+console.log(`Dado que foi removido: ${primeiroeElemento}`);
