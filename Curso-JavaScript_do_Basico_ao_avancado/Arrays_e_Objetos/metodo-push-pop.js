a162c28a9ba5ac0150c747e07287d80504a215cb
Ramon Valeriano <rrvaleriano@gmail.com>
2023-07-31 22:33:57 -0300

    Usando o pop no no array

---
+++ Curso-JavaScript_do_Basico_ao_avancado/Arrays_e_Objetos/metodo-push-pop.js
@@ -0,0 +1,16 @@
+/*
+    Aprendnedo a usar os metodos push e pop.
+*/
+
+// Lista ou Array.
+let pessoas = ["Mathes", "Ramon", "Milla"];
+
+// Todas as pessoas no array.
+console.log("Todas as pessoas no array: ");
+console.log(pessoas);
+
+// Deletando o último elemento e atribuindo a uma variável.
+let pessoaRemovida = pessoas.pop();
+console.log("Como ficou o Array: ");
+console.log(pessoas);
+console.log(`Dado excluído: ${pessoaRemovida}`);
