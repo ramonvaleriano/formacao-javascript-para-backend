ac6e58a973337167838f26e129123b3253291f3e
Ramon Valeriano <rrvaleriano@gmail.com>
2023-09-15 22:21:11 -0300

    Tipos Númericos para saber mais

---
+++ Curso-JavaScript_tipos_variaveis_e_funcoes/1-tiposPrimitivos/tipo-number-saber-mais.js
@@ -0,0 +1,25 @@
+/*
+    Tipos Primitivos
+
+    Tipo Númerico
+
+    Para Saber Mais.
+*/
+
+// Trabalhando com Números.
+const idade = 34;
+const pi = 3.14;
+
+console.log("Trabalhando com números: ");
+console.log(idade);
+console.log(pi);
+
+// Trabalhando com casos especificos de números.
+var numeroDez = 10;
+var numeroZero = 0;
+
+var divisaoInfinita = numeroDez / numeroZero;
+var casoNaN = numeroZero / numeroZero;
+
+console.log(`Resultado de uma divisão de 10 por 0: ${divisaoInfinita}`);
+console.log(`Resultado de uma divisão de zero por zero: ${casoNaN}`);
