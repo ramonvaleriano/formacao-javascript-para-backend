8e9d78c42b3a53835e7857afe2872d5409a746d5
Ramon Valeriano <rrvaleriano@gmail.com>
2023-09-15 21:41:14 -0300

    Revisando JavaScript, do inicio.

---
+++ Curso-JavaScript_tipos_variaveis_e_funcoes/1-tiposPrimitivos/tipo-number.js
@@ -0,0 +1,14 @@
+/*
+    Tipos Primitivos
+
+    Tipo Number.
+*/
+
+const meuNumero = 14;
+
+const fraseString = 'O valor desejado: '
+
+console.log(fraseString)
+console.log(meuNumero)
+
+console.log(`${fraseString}${meuNumero}`)
