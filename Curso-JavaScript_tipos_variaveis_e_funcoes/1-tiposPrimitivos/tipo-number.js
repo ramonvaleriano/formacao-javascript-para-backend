63f90c61492d2008ca155a8cf88eb77d5742a0cc
Ramon Valeriano <rrvaleriano@gmail.com>
2023-09-15 21:59:11 -0300

    Tipos Númericos

--- Curso-JavaScript_tipos_variaveis_e_funcoes/1-tiposPrimitivos/tipo-number.js
+++ Curso-JavaScript_tipos_variaveis_e_funcoes/1-tiposPrimitivos/tipo-number.js
@@ -17,7 +17,41 @@ console.log(`${fraseString}${meuNumero}`);
 const primeiroNumero = 1;
 const segundoNumero = 2;
 
+// Soma de dois números.
 const somaNumeros = primeiroNumero + segundoNumero;
 
 console.log("Validando soma: ");
 console.log(somaNumeros);
+
+// Multiplicação de dois números.
+const multiplicacaoDoisNumeros = primeiroNumero * segundoNumero;
+
+console.log(`A multiplicação de dois Números ${multiplicacaoDoisNumeros}`);
+
+// Subtração de dois Números.
+const subtracaoDoisNumeros = primeiroNumero - segundoNumero;
+
+console.log(`A subtração de dois números: ${subtracaoDoisNumeros}`);
+
+// Divisão de dois números.
+const divisaoDoisNumeros = primeiroNumero / segundoNumero;
+
+console.log(`A divisão dos dois números: ${divisaoDoisNumeros}`);
+
+// Resto de dois números.
+const restoDoisNumeros = primeiroNumero % segundoNumero;
+
+console.log(`Resto de dois números: ${restoDoisNumeros}`);
+
+// Números de Pontos Flutuantes.
+
+const numeroPontoFlutuante = 3.14;
+const pontoFlutuanteSemOZero = 0.5;
+
+console.log(`Número ponto Flutuante: ${numeroPontoFlutuante}`);
+console.log(`Número sem o zero antes: ${pontoFlutuanteSemOZero}`);
+
+// NaN -> Not A Number (Não é um número).
+const nanNumber = "Palavra" * segundoNumero;
+
+console.log(`NaN: ${nanNumber}`);
