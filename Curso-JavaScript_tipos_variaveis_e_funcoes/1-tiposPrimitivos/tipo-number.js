0b9e6f52132c970ac586aca23cec2e6455db7bb3
Ramon Valeriano <rrvaleriano@gmail.com>
2023-09-15 21:44:25 -0300

    Validando alguns tipos númericos

--- Curso-JavaScript_tipos_variaveis_e_funcoes/1-tiposPrimitivos/tipo-number.js
+++ Curso-JavaScript_tipos_variaveis_e_funcoes/1-tiposPrimitivos/tipo-number.js
@@ -4,11 +4,20 @@
     Tipo Number.
 */
 
+// Testadno variáveis númericas.
 const meuNumero = 14;
+const fraseString = "O valor desejado: ";
 
-const fraseString = 'O valor desejado: '
+console.log(fraseString);
+console.log(meuNumero);
 
-console.log(fraseString)
-console.log(meuNumero)
+console.log(`${fraseString}${meuNumero}`);
 
-console.log(`${fraseString}${meuNumero}`)
+// Validando variáveis númericas.
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const somaNumeros = primeiroNumero + segundoNumero;
+
+console.log("Validando soma: ");
+console.log(somaNumeros);
