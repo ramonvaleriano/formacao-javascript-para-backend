53257cd10d481eb6c2c2a6dfb348fad334627917
Ramon Valeriano <rrvaleriano@gmail.com>
2023-09-25 22:26:39 -0300

    Trabalhando com null e underfined

---
+++ Curso-JavaScript_tipos_variaveis_e_funcoes/1-tiposPrimitivos/tipo-null-underfined-saber-mais.js
@@ -0,0 +1,17 @@
+/**
+ * Tipos Primitivos:
+ * Tipo Null e Undefined.
+ */
+
+// Trabalhando com Null e Undefined.
+let variavelNull = null;
+let variavelUndefined;  
+
+if (!variavelNull){
+    console.log('Não há qualquer valor!')
+} else {
+    console.log('Há um valor aqui!')
+}
+
+console.log(variavelNull === variavelUndefined)
+console.log(variavelNull == variavelUndefined)
