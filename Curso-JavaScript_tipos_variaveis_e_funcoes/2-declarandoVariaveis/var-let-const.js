0d8c9aa764eece6e414e7ed652fa83e17291621a
Ramon Valeriano <rrvaleriano@gmail.com>
2023-09-28 21:12:43 -0300

    Usando o var para declarar variáveis.

---
+++ Curso-JavaScript_tipos_variaveis_e_funcoes/2-declarandoVariaveis/var-let-const.js
@@ -0,0 +1,20 @@
+/**
+ * Declarando Variáveis:
+ * Tipos: Var, Let e Const.
+ */
+
+var altura = 10;
+var comprimento = 30;
+
+var area = altura * comprimento;
+
+
+// Testando os resultados da variável do tipo var.
+console.log('Vamos analisar cada variável e seu resultado: ')
+console.log(`A sua altura: ${altura}`)
+console.log(`Temos o nosso comprimento: ${comprimento}`)
+console.log(`Temos a nossa area: ${area}`)
+
+// Declerando a var depois do uso dela. 
+console.log(`Var sendo declarada depois: ${varDepois}`)
+var varDepois = 33;
