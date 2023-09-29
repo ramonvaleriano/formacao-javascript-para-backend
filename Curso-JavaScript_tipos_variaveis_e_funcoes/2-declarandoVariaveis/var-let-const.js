a3d9d61525abf95dd215cf803de682c46860f6c3
Ramon Valeriano <rrvaleriano@gmail.com>
2023-09-28 21:29:17 -0300

    Trabalhando com o tipo de variavel let

--- Curso-JavaScript_tipos_variaveis_e_funcoes/2-declarandoVariaveis/var-let-const.js
+++ Curso-JavaScript_tipos_variaveis_e_funcoes/2-declarandoVariaveis/var-let-const.js
@@ -8,17 +8,15 @@ var comprimento = 30;
 
 var area = altura * comprimento;
 
-
 // Testando os resultados da variável do tipo var.
-console.log('Vamos analisar cada variável e seu resultado: ')
-console.log(`A sua altura: ${altura}`)
-console.log(`Temos o nosso comprimento: ${comprimento}`)
-console.log(`Temos a nossa area: ${area}`)
+console.log("Vamos analisar cada variável e seu resultado: ");
+console.log(`A sua altura: ${altura}`);
+console.log(`Temos o nosso comprimento: ${comprimento}`);
+console.log(`Temos a nossa area: ${area}`);
 
-// Declerando a var depois do uso dela. 
-console.log(`Var sendo declarada depois: ${varDepois}`)
+// Declerando a var depois do uso dela.
+console.log(`Var sendo declarada depois: ${varDepois}`);
 var varDepois = 33;
-
 
 // Analisasando dados com let.
 
@@ -26,10 +24,26 @@ let primeiroNumero = 10;
 let segundoNumero = 20;
 
 let soma = primeiroNumero + segundoNumero;
+let alturaY = 10;
+let comprimentoX = 5;
 
 // Testadno o resultado da let.
-console.log(`Testando as variáveis com let: `)
-console.log(`O primeiro número: ${primeiroNumero}`)
-console.log(`O segundo número: ${segundoNumero}`)
-console.log(`A soma dos números: ${soma}`)
+console.log("\n");
+console.log(`Testando as variáveis com let: `);
+console.log(`O primeiro número: ${primeiroNumero}`);
+console.log(`O segundo número: ${segundoNumero}`);
+console.log(`A soma dos números: ${soma}`);
+
+// Ainda com o let.
+let formaGeometrica = "retangulo";
+let areaGeo;
+
+if (formaGeometrica === "retangulo") {
+  areaGeo = comprimentoX * alturaY;
+} else if (formaGeometrica === "triangulo") {
+  areaGeo = (comprimentoX * alturaY) / 2;
+}
+console.log("\n");
+console.log(`A forma exigida: ${formaGeometrica}`);
+console.log(`A area: ${areaGeo}`);
 
