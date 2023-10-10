94e78ba969474fb776618fc822444f0bb40dbbf2
Ramon Valeriano <rrvaleriano@gmail.com>
2023-10-09 21:32:56 -0300

    Template string

---
+++ Curso-JavaScript_tipos_variaveis_e_funcoes/4-Operadores/templete-string.js
@@ -0,0 +1,13 @@
+/**
+ * Templete String.
+ */
+
+// Concatenando meu nome.
+const primeiroNome = 'Ramon'
+const espaco = ' '
+const nomeDoMeio = 'Rodrigues'
+const ultimoNome = 'Valeriano'
+
+const nomeCompleto = primeiroNome + espaco + nomeDoMeio + espaco + ultimoNome
+
+console.log(`Meu nome completo: ${nomeCompleto}`)
