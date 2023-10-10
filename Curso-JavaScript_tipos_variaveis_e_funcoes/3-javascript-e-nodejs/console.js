3489072e53b8f14efdab3861fe978faf68b31c62
Ramon Valeriano <rrvaleriano@gmail.com>
2023-10-09 21:04:05 -0300

    Javascript, nodejs  e console

---
+++ Curso-JavaScript_tipos_variaveis_e_funcoes/3-javascript-e-nodejs/console.js
@@ -0,0 +1,17 @@
+/**
+ * JavaScript e Node.js
+ * -> Console do JavaScript.
+ */
+
+// Testando usabilidade do console.
+
+console.time()
+console.log('Estudando as funcionabilidades do console.')
+console.log('Mostra mensagem no prompt de comando pelo nodejs.')
+
+console.error('Mostra o que foi dado como error.')
+console.info('Mostra uma mensagem de informação.')
+
+console.log('Se for enviar essas informações diretamente no prompt de comando, não terpa muita diferença, esses conceitos será mais definido quando estamos usando no front.')
+console.timeEnd()
+console.trace()
---
+++ Curso-JavaScript_tipos_variaveis_e_funcoes/3-javascript-e-nodejs/javascript-e-nodejs.js
@@ -0,0 +1,17 @@
+/**
+ * Javascript e NodeJs
+ */
+
+// Tipagem dinâmica.
+
+let variavelDinamica = 1234567;
+
+console.log(`A viriável: variavelDinamica: ${variavelDinamica} é do tipo: ${typeof variavelDinamica}`)
+
+variavelDinamica = 'Um nome qualquer!'
+
+console.log(`A viriável: variavelDinamica: ${variavelDinamica} é do tipo: ${typeof variavelDinamica}`)
+
+variavelDinamica = false
+
+console.log(`A viriável: variavelDinamica: ${variavelDinamica} é do tipo: ${typeof variavelDinamica}`)
