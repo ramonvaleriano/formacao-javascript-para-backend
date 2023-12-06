8f9ecb27ee5e8513d18ba15e5b6d974f2bb09055
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-12-06 09:17:33 -0300

    Funções generalizadas

---
+++ Curso-JavaScript_tipos_variaveis_e_funcoes/5-funcoes/funcoes-generealizadas.js
@@ -0,0 +1,12 @@
+/**
+ * Funções: Funções Generalizadas
+ */
+
+// Estudando funções de forma generalizada.
+function mensage(word) {
+  console.log(word);
+}
+
+// Vamos exibir textos:
+mensage("Hello, word!");
+mensage("Ramon Valeriano");
