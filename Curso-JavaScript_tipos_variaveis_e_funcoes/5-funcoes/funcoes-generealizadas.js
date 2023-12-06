c58006f1af797220683ab80544f4cce63ad8a6e8
Ramon Valeriano <ramon.valeriano@yandeh.com.br>
2023-12-06 09:25:14 -0300

    SOma de dois números

--- Curso-JavaScript_tipos_variaveis_e_funcoes/5-funcoes/funcoes-generealizadas.js
+++ Curso-JavaScript_tipos_variaveis_e_funcoes/5-funcoes/funcoes-generealizadas.js
@@ -10,3 +10,16 @@ function mensage(word) {
 // Vamos exibir textos:
 mensage("Hello, word!");
 mensage("Ramon Valeriano");
+
+// Uma funçaõ de soma de dois números mockados, sem parâmetros.
+function sumTwoNUmbers() {
+  const number1 = 10;
+  const number2 = 30;
+  const result = number1 + number2;
+
+  return result;
+}
+
+// Usando as duas funções ao mesmo tempo.
+const sumaTwo = sumTwoNUmbers();
+mensage(sumaTwo);
