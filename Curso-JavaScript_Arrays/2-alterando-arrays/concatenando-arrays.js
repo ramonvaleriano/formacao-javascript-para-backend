254eed5886b9a74490415184ed2b3c375f9ff6e9
Ramon Valeriano <rrvaleriano@gmail.com>
2024-01-23 23:48:05 -0300

    Concatenando arrays

---
+++ Curso-JavaScript_Arrays/2-alterando-arrays/concatenando-arrays.js
@@ -0,0 +1,18 @@
+/**
+ * Alterando Arrays.
+ * Concatenando Arrays.
+ */
+
+// Dados para serem trabalhados.
+
+const salaJS = ["Evaldo", "Camis", "Mari"];
+const salaPython = ["Ju", "Leo", "Raquel"];
+
+// Concatenando os dados.
+
+const salaTotal = salaJS.concat(salaPython);
+
+console.log(`A sala JS: ${salaJS}`)
+console.log(`A sala Python: ${salaPython}`)
+
+console.log(`A sala total: ${salaTotal}`)
