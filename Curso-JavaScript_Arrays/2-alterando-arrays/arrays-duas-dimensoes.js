3f8b4fac5b4d490231b691b671c367c3d19f61f1
Ramon Valeriano <rrvaleriano@gmail.com>
2024-01-23 23:55:54 -0300

    Arrays concatenados

---
+++ Curso-JavaScript_Arrays/2-alterando-arrays/arrays-duas-dimensoes.js
@@ -0,0 +1,11 @@
+/**
+ * Alterando arrays.
+ * Concatenando arrays.
+ */
+
+// Dados para se trabalhar.
+
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+
+const listaJuntas = [alunos, medias]
