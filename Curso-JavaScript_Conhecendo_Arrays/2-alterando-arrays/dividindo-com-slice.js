71c9e71622f8590750ecb51055872d42895af546
Ramon Valeriano <rrvaleriano@gmail.com>
2024-01-30 14:39:45 -0300

    Dividindo dados com slice

---
+++ Curso-JavaScript_Conhecendo_Arrays/2-alterando-arrays/dividindo-com-slice.js
@@ -0,0 +1,53 @@
+/**
+ * Alterando Arrays
+ * Dividindo Arrays com slice
+ */
+
+// Dados para o uso.
+
+const listaEstudantes = [
+  "João",
+  "Juliana",
+  "Ana",
+  "Caio",
+  "Lara",
+  "Marjorie",
+  "Guilherme",
+  "Aline",
+  "Fabiana",
+  "André",
+  "Carlos",
+  "Paulo",
+  "Bia",
+  "Vivian",
+  "Isabela",
+  "Vinícius",
+  "Renan",
+  "Renata",
+  "Daisy",
+  "Camilo",
+];
+
+// Criando uma função para dividir a lista em duas partes.
+function dividirTurmas(listaDeEstudantes){
+    if(!Array.isArray(listaDeEstudantes) && listaDeEstudantes.length <= 0) return [[],[]]
+    let turma1 = new Array();
+    let turma2 = new Array();
+    
+    if (listaDeEstudantes.length % 2 === 0){
+        turma1 = listaDeEstudantes.slice(0, listaDeEstudantes.length / 2)
+        turma2 = listaDeEstudantes.slice(listaDeEstudantes.length /  2)
+    } else{
+        const meio = Math.ceil(listaDeEstudantes.length / 2)
+        turma1 = listaDeEstudantes.slice(0, meio)
+        turma1 = listaDeEstudantes.slice(meio)
+    }
+    return [turma1, turma2]
+}
+
+// Testanoo os dados.
+const [turma1, turma2] = dividirTurmas(listaEstudantes)
+
+console.log(listaEstudantes)
+console.log(turma1)
+console.log(turma2)
