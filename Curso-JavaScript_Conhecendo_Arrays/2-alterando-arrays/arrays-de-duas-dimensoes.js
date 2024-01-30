77ec2a445c284f6c75df6355a60734d08215b9e6
Ramon Valeriano <rrvaleriano@gmail.com>
2024-01-30 15:54:50 -0300

    Matrizes

---
+++ Curso-JavaScript_Conhecendo_Arrays/2-alterando-arrays/arrays-de-duas-dimensoes.js
@@ -0,0 +1,13 @@
+/**
+ * Alterando Arrays
+ * Arrays de Duas Dimensões
+ */
+
+// Dados para uso.
+const alunos = ["João", "Juliana", "Caio", "Ana"];
+const medias = [10, 8, 7.5, 9];
+
+// Arrays duplo.
+const arrayDuplo = [alunos, medias];
+
+console.log(`Alunos: ${arrayDuplo[0]}\nMedias: ${arrayDuplo[1]}`);
