4ba8c9100a967b2b6681b6668f71fd80d02bd7b8
Ramon Valeriano <rrvaleriano@gmail.com>
2024-02-05 10:18:35 -0300

    Desafio 05

--- Curso-JavaScript_Conhecendo_Arrays/2-alterando-arrays/desafio5.js
+++ Curso-JavaScript_Conhecendo_Arrays/2-alterando-arrays/desafio5.js
@@ -7,3 +7,20 @@
  */
 
 // Função para resolução da questão.
+function listaTresLinhasTresColunas() {
+  const arrayGeral = new Array();
+  for (let i = 1; i <= 3; i++) {
+    console.log(`Loop: ${i}`);
+    let arrayTemporario = new Array();
+    for (let l = 1; l <= 3; l++) {
+      arrayTemporario.push(l);
+    }
+    console.log(`Array criado: ${arrayTemporario}`);
+    arrayGeral.push(arrayTemporario);
+  }
+  return arrayGeral;
+}
+
+// Executando a questão.
+const arrayGeral = listaTresLinhasTresColunas();
+console.log(arrayGeral);
