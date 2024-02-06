afba339ffdb8d195e64680b8b568b7c28ce0d433
Ramon Valeriano <rrvaleriano@gmail.com>
2024-02-06 14:56:27 -0300

    Desafio03

--- Curso-JavaScript_Conhecendo_Arrays/3-lacos-de-repeticao/desafio2.js
+++ Curso-JavaScript_Conhecendo_Arrays/3-lacos-de-repeticao/desafio2.js
@@ -5,31 +5,30 @@
 
 // Criando função para resolver o desafio.
 function validadeDoArray(arrayDados) {
-    // Função para validar se é uma lista e se não é vazia.
-    if (!Array.isArray(arrayDados) || arrayDados.length <= 0) {
-        console.log("Não é possível validar esse dados.")
-      return false;
-    }
-    console.log("Esse dado é valido.")
-    return true;
+  // Função para validar se é uma lista e se não é vazia.
+  if (!Array.isArray(arrayDados) || arrayDados.length <= 0) {
+    console.log("Não é possível validar esse dados.");
+    return false;
   }
+  console.log("Esse dado é valido.");
+  return true;
+}
 
-
-function exibindoCadaElemento(arrayDados){
-    // Função para exibir cada elemento.
-    if (validadeDoArray(arrayDados)){
-        let soma = 0
-        for (let i = 0; i < arrayDados.length; i++ ){
-          console.log(`${i} -> ${arrayDados[i]}`)
-          soma += arrayDados[i]
-        }
-        console.log(`A soma total dos dados: ${soma}`)
-        return soma
+function exibindoCadaElemento(arrayDados) {
+  // Função para exibir cada elemento.
+  if (validadeDoArray(arrayDados)) {
+    let soma = 0;
+    for (let i = 0; i < arrayDados.length; i++) {
+      console.log(`${i} -> ${arrayDados[i]}`);
+      soma += arrayDados[i];
     }
-    console.log("Não é possível fazer o uso dos dados.")
-    return null
+    console.log(`A soma total dos dados: ${soma}`);
+    return soma;
+  }
+  console.log("Não é possível fazer o uso dos dados.");
+  return null;
 }
 
 // Resolvendo a questão.
-const numeros = [3, 8, 12, 5, 6, 10, 7, 2, 9, 14]
-const soma = exibindoCadaElemento(numeros)
+const numeros = [3, 8, 12, 5, 6, 10, 7, 2, 9, 14];
+const soma = exibindoCadaElemento(numeros);
--- Curso-JavaScript_Conhecendo_Arrays/3-lacos-de-repeticao/desafio3.js
+++ Curso-JavaScript_Conhecendo_Arrays/3-lacos-de-repeticao/desafio3.js
@@ -1,6 +1,6 @@
 /**
  * Laços de Repetições.
- * 1 - Escreva um código que utilize o loop for/of para iterar e imprimir cada elemento de um array.
+ * 3 - Crie uma função que receba uma array de números inteiros e retorne a soma dos elementos.
  */
 
 // Criando função para resolver o desafio.
@@ -14,23 +14,21 @@ function validadeDoArray(arrayDados) {
   return true;
 }
 
-function exibindoCadaElemento(arrayDados) {
+function somaDeTodosOsNumeros(arrayDados) {
   // Função para exibir cada elemento.
   if (validadeDoArray(arrayDados)) {
-    let soma = 0;
-    let contador = 1;
-    for (dado of arrayDados) {
-      console.log(`${contador} -> ${dado}`);
-      soma += dado;
-      contador += 1;
-    }
-    console.log(`A soma total dos dados: ${soma}`);
-    return soma;
+    const somaDosNumeros = arrayDados.reduce((soma, valor) => {
+      soma += valor;
+      return soma;
+    }, 0);
+    console.log(`A soma total dos dados: ${somaDosNumeros}`);
+    return somaDosNumeros;
   }
+
   console.log("Não é possível fazer o uso dos dados.");
   return null;
 }
 
 // Resolvendo a questão.
 const numeros = [3, 8, 12, 5, 6, 10, 7, 2, 9, 14];
-const soma = exibindoCadaElemento(numeros);
+const soma = somaDeTodosOsNumeros(numeros);
