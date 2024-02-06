11fe033ea5ca229b92ef533e2b038da99e511557
Ramon Valeriano <rrvaleriano@gmail.com>
2024-02-06 14:59:39 -0300

    Iniciando desafio04

---
+++ Curso-JavaScript_Conhecendo_Arrays/3-lacos-de-repeticao/desafio4.js
@@ -0,0 +1,23 @@
+/**
+ * Laços de Repetições.
+ * 4 - Crie uma função que receba uma array de números inteiros e retorne o menor valor e o maior valor da array, no seguinte formato: 
+ * 'o menor número é X e o maior número é Y'.
+ */
+
+// Criando função para resolver o desafio.
+function validadeDoArray(arrayDados) {
+  // Função para validar se é uma lista e se não é vazia.
+  if (!Array.isArray(arrayDados) || arrayDados.length <= 0) {
+    console.log("Não é possível validar esse dados.");
+    return false;
+  }
+  console.log("Esse dado é valido.");
+  return true;
+}
+
+function maiorMenorValorDaLista(arrayDados){
+  if (!validadeDoArray) return null
+}
+
+// Resolvendo a questão.
+const numeros = [3, 8, 12, 5, 6, 10, 7, 2, 9, 14];
