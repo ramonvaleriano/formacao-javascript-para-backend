6e8b2128b3890ee8aeff137ec5666afa617a76a9
Ramon Valeriano <rrvaleriano@gmail.com>
2024-02-09 12:27:20 -0300

    Criando função para validar Array

---
+++ Curso-JavaScript_Conhecendo_Arrays/4-funcoes-callback/media-foreach.js
@@ -0,0 +1,18 @@
+/**
+ * Funções CallBack
+ * Media forEach
+ */
+
+// Função para o uso no estudo.
+function validaArray(arrayDados) {
+  if (!Array.isArray(arrayDados) || arrayDados.length <= 0) {
+    console.log("Essado dado não pode ser validado");
+    return false;
+  }
+  console.log("Dado validade com sucesso");
+  return true;
+}
+
+const mediaNotas = (arrayDados) => {};
+
+//Dados para user no teste.
