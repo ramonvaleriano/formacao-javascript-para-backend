540d5b7a693bec14124d4ea3bde2a7651066775a
Ramon Valeriano <rrvaleriano@gmail.com>
2024-11-20 12:38:41 -0300

    Operadores Comparativos

---
+++ Curso-JavaScript-Variáveis-e-Funcoes/4-Operadores/1-operadores-comparacao.js
@@ -0,0 +1,11 @@
+/**
+ * Operadores de Comparação
+ */
+import funcoes from '../funcoes/funcoes.js'
+const {exibirMensagem} = funcoes;
+
+const variavel1 = 5
+const variavel2 = '5'
+
+exibirMensagem(variavel1 == variavel2)
+exibirMensagem(variavel1 === variavel2)
