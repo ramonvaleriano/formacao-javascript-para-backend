44010ca42c0a6b626352144f8cd11a9ad1a9941f
Ramon Valeriano <rrvaleriano@gmail.com>
2024-11-22 14:48:18 -0300

    Unindo duas arrays

---
+++ Curso-JavaScript-Arrays/2-alterando-arrays/3-concatenando-salas.js
@@ -0,0 +1,27 @@
+/**
+ * Concatenando Salas, juntandos duas salas
+ */
+
+// Arrys para para duas salas.
+
+const salaJS = ['Evaldo', 'Camis', 'Mari'];
+const salaPython = ['Ju', 'Leo', 'Raquel'];
+
+const salaJS2 = ['Evaldo', 'Camis', 'Mari'];
+const salaPython2 = ['Ju', 'Leo', 'Raquel'];
+
+console.log("*****************")
+console.log(`SalaJS: ${salaJS}`)
+console.log(`salaPython: ${salaPython}`)
+
+// Como se juntas salas.
+const superSala = [...salaJS, ...salaPython]
+console.log(`Como junstas duas salas: ${superSala}`)
+
+// Sala Concatenada.
+salaPython.concat(salaJS)
+console.log(`Duas salas concatenadas com .concat: ${superSala}`)
+
+// Jutando salas com push.
+salaPython2.push(...salaJS)
+console.log(`Duas salas concatenadas com .push: ${superSala}`)
