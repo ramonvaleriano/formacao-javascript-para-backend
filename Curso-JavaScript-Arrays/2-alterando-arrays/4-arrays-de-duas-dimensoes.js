a964c7ddfcd0b52b4269b2955a31a9ff7d592a0f
Ramon Valeriano <rrvaleriano@gmail.com>
2024-11-22 16:07:52 -0300

    Trabalhando com arrays duplos

---
+++ Curso-JavaScript-Arrays/2-alterando-arrays/4-arrays-de-duas-dimensoes.js
@@ -0,0 +1,15 @@
+/**
+ * Array de Duas dimensões
+ */
+
+// Arrays que iremos trabalhar
+const alunos = ['João', 'Juliana', 'Caio', 'Ana'];
+const medias = [10, 8, 7.5, 9];
+
+// Tornando array de duas dimensões
+const lista = [alunos, medias]
+
+// 1° Forme de acessar elas.
+for (i = 0; i < lista[0].length; i++){
+    console.log(`Aluno: ${lista[0][i]} -> Nota: ${lista[1][i]}`)
+}
