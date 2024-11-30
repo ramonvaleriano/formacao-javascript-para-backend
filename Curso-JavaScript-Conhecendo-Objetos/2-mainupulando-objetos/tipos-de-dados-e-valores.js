85f0a04d4873f037c71e738e775488ff7eddfb51
Ramon Valeriano <rrvaleriano@gmail.com>
2024-11-30 12:05:31 -0300

    Curso-JavaScript-Conhecendo-Objetos\2-mainupulando-objetos

---
+++ Curso-JavaScript-Conhecendo-Objetos/2-mainupulando-objetos/tipos-de-dados-e-valores.js
@@ -0,0 +1,16 @@
+/**
+ * Tipos de dados e valores
+ */
+
+// Dados para se trabalhar com objetos
+const estudante = {
+  nome: "José Silva",
+  idade: 32,
+  cpf: "12312312312",
+  turma: "JavaScript",
+  bolsista: true,
+  telefones: ["551199999998", "551199999993"],
+};
+
+console.log(estudante.bolsista)
+console.log(estudante.telefones[0])
