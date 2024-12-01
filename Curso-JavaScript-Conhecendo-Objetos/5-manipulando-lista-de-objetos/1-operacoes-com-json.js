3fb02b50df3a5b574dbb67852bb3a2c6b91ee997
Ramon Valeriano <rrvaleriano@gmail.com>
2024-12-01 16:58:32 -0300

    Melhorando o código para coeltar dentro de uma lista dentro de uma objeto

--- Curso-JavaScript-Conhecendo-Objetos/5-manipulando-lista-de-objetos/1-operacoes-com-json.js
+++ Curso-JavaScript-Conhecendo-Objetos/5-manipulando-lista-de-objetos/1-operacoes-com-json.js
@@ -6,10 +6,11 @@ const estudantes = require("../arquivos-alura/estudantes.json");
 
 function buscandoInformacao(listaDeDados, chave, valor) {
     const dadoFiltrado = listaDeDados.find((estudante) => {
-        return estudante[chave] === valor;
+        return estudante[chave].includes(valor);
     });
 
     return dadoFiltrado;
 }
 
 console.log(buscandoInformacao(estudantes, "nome", "Amye"))
+console.log(buscandoInformacao(estudantes, "telefone", "9045673092"))
